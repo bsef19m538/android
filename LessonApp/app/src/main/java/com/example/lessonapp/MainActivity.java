30ae808923590573535d5089f636a2e0f7933b64
bsef19m538 <Bsef19m534>
2022-09-12 22:45:16 -0700

    MC Home work

---
+++ LessonApp/app/src/main/java/com/example/lessonapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.lessonapp;
+
+import static com.example.lessonapp.R.drawable.balloon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button,button2,button3;
+        button = findViewById(R.id.button);
+        button2=findViewById(R.id.button2);
+        button3=findViewById(R.id.button3);
+
+        button.setOnClickListener(view -> {
+            Uri uri = Uri.parse("https://github.com/Bsef19m534/MobileComputing");
+            Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+            startActivity(intent);
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+
+                startActivity(intent);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this,Q1.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ LessonApp/app/src/main/java/com/example/lessonapp/setQ.java
@@ -0,0 +1,103 @@
+package com.example.lessonapp;
+
+public class setQ
+
+        {
+                public static int question[] ={
+                      R.drawable.ice_cream,
+                        R.drawable.peacock,
+                        R.drawable.hat,
+                        R.drawable.zebra,
+                        R.drawable.cat,
+                        R.drawable.apple,
+                        R.drawable.balloon,
+                        R.drawable.duck,
+                        R.drawable.egg,
+                        R.drawable.fish,
+                        R.drawable.glass,
+                        R.drawable.jelly,
+                        R.drawable.kite,
+                        R.drawable.lemon,
+                        R.drawable.mango,
+                        R.drawable.nest,
+                        R.drawable.orange,
+                        R.drawable.question,
+                        R.drawable.rose,
+                        R.drawable.sun,
+                        R.drawable.tree,
+                        R.drawable.umbrella,
+                        R.drawable.violin,
+                        R.drawable.watermelon,
+                        R.drawable.xylophone,
+                        R.drawable.yogurt,
+
+
+
+
+
+
+
+
+                };
+
+                public static String choices[][] = {
+                        {"Ice Cream","Apple","Jelly","Duck"},
+                        {"Umbrella","Xylophone","Peacock","Lemon"},
+                        {"Egg","Tree","Rose","Hat"},
+                        {"Balloon","Glass","Zebra","Sun"},
+                        {"Yogurt","Orange","Violin","Cat"},
+                        {"Apple","Xylophone","Egg","Tree"},
+                        {"Glass","Mango","Balloon","Jelly"},
+                        {"Ice Cream","Duck","Nest","Question"},
+                        {"Sun","Umbrella","Rose","Egg"},
+                        {"Kite","Apple","Fish","Cat"},
+                        {"Glass","Balloon","Yogurt","Peacock"},
+                        {"Zebra","Jelly","Lemon","Violin"},
+                        {"Apple","Tree","Watermelon","Kite"},
+                        {"Yogurt","Lemon","Zebra","Cat"},
+                        {"Mango","Orange","Violin","Ice cream"},
+                        {"Yogurt","Orange","Nest","Cat"},
+                        {"Yogurt","Orange","Violin","Orange"},
+                        {"Yogurt","Question","Violin","Cat"},
+                        {"Yogurt","Orange","Rose","Cat"},
+                        {"Sun","Orange","Violin","Cat"},
+                        {"Yogurt","Tree","Violin","Cat"},
+                        {"Yogurt","Orange","Violin","Umbrella"},
+                        {"Yogurt","Orange","Violin","Cat"},
+                        {"Yogurt","Watermelon","Violin","Cat"},
+                        {"Yogurt","Orange","Xylophone","Cat"},
+                        {"Yogurt","Orange","Balloon","Cat"},
+
+                };
+
+                public static String correctAnswers[] = {
+                        "Ice Cream",
+                        "Peacock",
+                        "Hat",
+                        "Zebra",
+                        "Cat",
+                        "Apple",
+                        "Balloon",
+                        "Duck",
+                        "Egg",
+                        "Fish",
+                        "Glass",
+                        "Jelly",
+                        "Kite",
+                        "Lemon",
+                        "Mango",
+                        "Nest",
+                        "Orange",
+                        "Question",
+                        "Rose",
+                        "Sun",
+                        "Tree",
+                        "Umbrella",
+                        "Violin",
+                        "Watermelon",
+                        "Xylophone",
+                        "Yogurt",
+                        "Zebra"
+
+                };
+}
